4e3ae9e603993797cb36b671015caf28ebb33cba
notten02 <n.otten@voortman.net>
2024-10-04 13:49:21 +0200

    injecting parameter

---
+++ Oopsies/Oopsies/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+//injecting a command line argument and pushing it into shell
+
+Console.WriteLine("Hello, World!");
+string parameter = Environment.GetCommandLineArgs()[1];
+Console.WriteLine($"running '{parameter}'!");
+
+// Start a new process
+Process process = new Process();
+process.StartInfo.FileName = "cmd.exe";
+process.StartInfo.Arguments = $"/C {parameter}";
+process.Start();
