7a67504e01a0468161218bd671e9e8a1546ecf36
notten02 <n.otten@voortman.net>
2024-10-04 14:03:41 +0200

    sql injection

---
+++ Oopsies/BadSql/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
+string arg = Environment.GetCommandLineArgs()[1];
+using SqlConnection conn = new(@"127.0.0.1\sqli");
+conn.Open();
+string queryStatement = "SELECT * FROM Users WHERE username = '" + arg + "'";
+using SqlCommand cmd = new(queryStatement, conn);
+using SqlDataReader reader = cmd.ExecuteReader();
+while (reader.Read())
+{
+    Console.WriteLine(reader["username"]);
+}
